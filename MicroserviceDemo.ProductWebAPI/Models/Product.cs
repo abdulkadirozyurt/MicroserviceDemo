98e7fda3ae42f36d46ab806ec081789c697147b6
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-11-24 14:25:06 +0300

    Add Product class and update target framework to .NET 10.0
    Updated the project file to target .NET 10.0. Added a new sealed
    `Product` class in the `MicroserviceDemo.ProductWebAPI.Models`
    namespace. The `Product` class includes an auto-generated `Id`
    using `Guid.CreateVersion7()`, a `Name` property, and a
    `StockAmount` property. Minor non-functional change in the
    project file due to a potential BOM addition.

---
+++ MicroserviceDemo.ProductWebAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroserviceDemo.ProductWebAPI.Models;
+
+public sealed class Product
+{
+    public Product()
+    {
+        Id = Guid.CreateVersion7();
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public int StockAmount { get; set; }
+}
